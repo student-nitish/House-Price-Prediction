1d298cf23ca50d2fe95670e3fe0a07ad9ecdd7d1
Nitish Kumar Rajak <nrajak16@gmail.com>
2026-06-05 15:08:18 +0530

    update

--- server/util.py
+++ server/util.py
@@ -46,7 +46,7 @@ def get_data_columns():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+    # print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    # print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    # print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    # print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
